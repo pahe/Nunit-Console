1524c5b16cfe916b38ca67b6309e2e21319e8fcd
Patrik Hermansson <patrik.hermansson@gmail.com>
2024-05-03 05:15:25 +0200

    Init

---
+++ NUnitConsole/NUnitConsole/ConnectionFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using NUnit.Framework;
+using Testcontainers.MsSql;
+
+namespace NUnitConsole
+{
+    [TestFixture]
+    public class ConnectionFixture
+    {
+        [Test]
+        [Ignore("")]
+        public void TestConnection()
+        {
+            var connectionString = "connection";
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        [Test]
+        public async Task TestConnectionWithContainers()
+        {
+            //https://dotnet.testcontainers.org/modules/mssql/
+            //You need to have docker installed in the machine that running the test. 
+
+            MsSqlContainer sqlContainer = new MsSqlBuilder().Build();
+            await sqlContainer.StartAsync();
+
+            var connection = new SqlConnection(sqlContainer.GetConnectionString());
+            connection.Open();
+        }
+    }
+}
---
+++ run-console.bat
@@ -0,0 +1,1 @@
+.\NunitConsoleRunner\bin\net8.0\nunit3-console.exe .\NUnitConsole\NUnitConsole\bin\Debug\net8.0\NUnitConsole.dll
